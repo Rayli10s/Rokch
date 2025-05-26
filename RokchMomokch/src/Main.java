3acd421b0d8463df513a1205dbb21df3cc827505
MouadElg <132202955+Rayli10s@users.noreply.github.com>
2025-05-26 16:30:04 -0700

    First change

--- RokchMomokch/src/Main.java
+++ RokchMomokch/src/Main.java
@@ -3,5 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("x");
+        System.out.println(22);
+        System.out.println("Intissar");
     }
 }
